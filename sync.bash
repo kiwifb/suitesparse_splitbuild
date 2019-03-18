919c314b4d1026ee10d7258946d4d228b7ee72d2
François Bissey <frp.bissey@gmail.com>
2019-03-19 09:54:35 +1300

    some more versioning

--- sync.bash
+++ sync.bash
@@ -8,4 +8,4 @@ for macro in ax_blas ax_lapack; do
     wget http://git.savannah.gnu.org/cgit/autoconf-archive.git/plain/m4/${macro}.m4 -O addons/m4/${macro}.m4
 done
 
-tar xf SuiteSparse.tar.gz
+tar xf SuiteSparse-5.4.0.tar.gz
